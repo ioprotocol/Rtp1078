c1adc56e1b016725630a59b623da6998a08c1f45
xsy <845158228@qq.com>
2020-03-30 21:28:21 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <array>
+
+int main() {
+    std::array<int, 3> a1{{1, 2, 3}};
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << a1.size() << std::endl;
+    return 0;
+}
